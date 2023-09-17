39fde7e9e4eee1995aae516bb93d68b3c0b597f5
Felix Jumason <felixkent360@gmail.com>
2023-09-17 17:26:05 +0300

    hero

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -2,10 +2,12 @@ import bgWhite from '../assets/bgWhite.png';
 import oasis from '../assets/oasis logo.svg';
 import transmission from '../assets/transmission.png';
 import router from '../assets/router.png';
+import kenyan from '../assets/kenyan.jpeg'
 export default{
 transmission,
 bgWhite,
 router,
 oasis,
+kenyan
 
 };
--- frontend_react/src/container/Header.jsx
+++ frontend_react/src/container/Header.jsx
@@ -1,31 +1,97 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { FaTwitter, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import { images } from '../constants';
 
 const Header = () => {
+  const imageSrc = images.kenyan; // Replace with your image source
+
+  const imageVariants = {
+    initial: {
+      opacity: 0,
+      y: -20,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <div className="bg-gray-100 py-16 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center">
-        <div className="lg:w-1/2 text-center lg:text-left">
-          <h1 className="text-4xl tracking-tight font-extrabold text-black sm:text-5xl md:text-6xl">
-            Welcome to <span className="text-red-500">Oasis</span> <span className='text-blue-500'>Telecommunication</span>
-          </h1>
-          <p className="mt-3 max-w-md mx-auto lg:mx-0 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            Your Reliable Internet Service Provider
-          </p>
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+    {/* On large screens, the image is larger on the left */}
+    <div className="md:flex md:items-center md:justify-between">
+      {/* Image section */}
+      <motion.div
+        className="md:w-1/2 rounded-full overflow-hidden w-64 h-64 mb-6 md:mb-0 md:mr-4"
+        initial="initial"
+        animate="animate"
+        variants={imageVariants}
+      >
+        <img
+          src={imageSrc}
+          alt="Header"
+          className="w-full h-full object-cover grayscale"
+        />
+      </motion.div>
+  
+      {/* Content section */}
+      <div className="md:w-1/2 text-center md:text-left">
+        <h1 className="text-2xl font-bold">Hello 👋 <br />I am Felix Jumas</h1>
+        <p className="text-lg">Frontend Developer</p>
+  
+        {/* Buttons */}
+        <div className="my-4 space-x-4 flex justify-center">
+          <button className="border border-black bg-white hover:bg-blue-600 text-black font-bold py-2 px-4 rounded">
+            Download CV
+          </button>
+          <button className="border border-black bg-white hover:bg-blue-600 text-black font-bold py-2 px-4 rounded">
+            Contact Info
+          </button>
         </div>
-        <div className="lg:w-1/2 mt-10">
-          <motion.img
-            src={images.oasis}
-            alt="Oasis Telecommunication"
-            className="rounded-full h-32 w-32 mx-auto lg:mx-0"
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ duration: 1}}
-          />
+  
+        {/* Social Icons */}
+        <div className="flex space-x-4">
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600"
+          >
+            <FaTwitter className="text-2xl" />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600"
+          >
+            <FaLinkedin className="text-2xl" />
+          </a>
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600"
+          >
+            <FaGithub className="text-2xl" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600"
+          >
+            <FaInstagram className="text-2xl" />
+          </a>
         </div>
       </div>
     </div>
+  </div>
+  
   );
 };
 
