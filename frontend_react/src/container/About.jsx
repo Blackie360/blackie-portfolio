b7185fe93bf953be4589c4c273321ecdc2c21134
Felix Jumason <felixkent360@gmail.com>
2023-09-17 19:12:07 +0300

    about

--- frontend_react/src/container/About.jsx
+++ frontend_react/src/container/About.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 import { motion } from 'framer-motion'; 
 import { images } from '../constants';
 
+const abouts = [
+  {title:'Frontend Development ', description: 'front end development is the development of those elements of a website that the customer sees and interacts with directly. It is a combination of programming skills (knowing which program to choose and when to use it), HTML, CSS and JavaScript.', imgUrl: images.router},
+  {title:'Frontend Development ', description: 'front end development is the development of those elements of a website that the customer sees and interacts with directly. It is a combination of programming skills (knowing which program to choose and when to use it), HTML, CSS and JavaScript.', imgUrl: images.router},
+  {title:'Frontend Development ', description: 'front end development is the development of those elements of a website that the customer sees and interacts with directly. It is a combination of programming skills (knowing which program to choose and when to use it), HTML, CSS and JavaScript.', imgUrl: images.router},
+
+];
+
 const About = () => {
   
 
   return (
    <>
    <h2 className='text-xl'> The greatest <span className='text-orange-600 '> ideas </span>are the <span className='text-orange-600 '>simplest.</span></h2>
+   <div>
+    {abouts.map((about, index) => (
+      <motion.div
+      whileInView={{opacity: 1}}
+      whileHover={{scale: 1.1}}
+      transition={{duration: 0.5, type: 'tween'}}
+      className=''
+      key={about.title + index}
+      >
+        <img src={about.imgUrl} alt={about.title}/>
+        <h2>{about.title}</h2>
+        <p>{about.description}</p>
+      </motion.div>
+    ))}
+   </div>
    </>
   );
 };
