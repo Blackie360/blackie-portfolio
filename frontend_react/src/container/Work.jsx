1606ae0c569d8ec0a3be14aaef8e1979babf1cd8
Felix Jumason <felixkent360@gmail.com>
2023-09-18 17:37:19 +0300

    filter through dynamic images

--- frontend_react/src/container/Work.jsx
+++ frontend_react/src/container/Work.jsx
@@ -7,19 +7,30 @@ const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
-  const [filterWork, setFilteredWork] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
       setWorks(data);
-      setFilteredWork(data);
+      setFilterWork(data);
     });
   }, []);
 
   const handleWorkFilter = (item) => {
-    // You can implement filter logic here based on the selected item
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
   };
 
   return (
@@ -50,15 +61,34 @@ const Work = () => {
         {filterWork.map((work, index) => (
           <div key={index} className="bg-white p-4 border rounded shadow-md">
             <img src={urlFor(work.imgUrl)} alt={work.name} className="w-full h-40 object-cover mb-4" />
-            <h3 className="text-lg font-semibold mb-2">{work.name}</h3>
-            <p className="text-gray-600">{work.description}</p>
-            <div className="mt-4 flex space-x-2">
-              <a href={work.githubUrl} target="_blank" rel="noopener noreferrer">
-                <AiFillGithub className="text-gray-700 hover:text-blue-500" />
+            <div className="flex justify-center space-x-4">
+              <a href={work.projectLink} target="_blank" rel="noopener noreferrer">
+                <motion.div
+                  whileHover={{ scale: [0, 0.9] }}
+                  whileInView={{ scale: [0, 1] }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <AiFillEye className="text-3xl" />
+                </motion.div>
               </a>
-              <a href={work.demoUrl} target="_blank" rel="noopener noreferrer">
-                <AiFillEye className="text-gray-700 hover:text-blue-500" />
+              <a href={work.codetLink} target="_blank" rel="noopener noreferrer">
+                <motion.div
+                  whileHover={{ scale: [0, 0.9] }}
+                  whileInView={{ scale: [0, 1] }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <AiFillGithub className="text-3xl" />
+                </motion.div>
               </a>
+            </div>
+            <div className="text-center mt-4">
+              <h4 className="text-lg font-semibold mb-2">{work.title}</h4>
+              <p className="text-gray-600">{work.description}</p>
+              <div className="flex justify-center mt-4">
+                <div className="app__work-tag bg-gray-300 px-2 py-1 rounded">
+                  <p className="text-sm text-gray-700">{work.tags[1]}</p>
+                </div>
+              </div>
             </div>
           </div>
         ))}
