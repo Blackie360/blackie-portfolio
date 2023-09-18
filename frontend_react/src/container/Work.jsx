fb084f7304823c9f8a6cb9b2ff480b12cc39a66e
Felix Jumason <felixkent360@gmail.com>
2023-09-18 15:41:57 +0300

    portfolio

--- frontend_react/src/container/Work.jsx
+++ frontend_react/src/container/Work.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {AiFillGithub, AifillEye} from 'react-icons/ai'
+import {motion} from 'framer-motion'
+import {urlFor, Client} from '../client'
 
 const Work = () => {
+  const [activeFilter, setactivefilter] = useState('All')
+  const handleWorkFilter = (item) => {
+
+  }
   return (
-    <div>Work</div>
+ <> 
+ <h2>portfolio section </h2>
+ <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+ </>
+
+
+ 
   )
 }
 
