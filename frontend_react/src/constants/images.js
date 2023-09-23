eb27e287c5656e2a58a7ef7990f07ea9f141fd89
Felix Jumason <felixkent360@gmail.com>
2023-09-23 14:17:41 +0300

    contact page

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -4,13 +4,15 @@ import cyber from '../assets/cyber.png';
 import developer from '../assets/developer.png';
 import problem from '../assets/problem.png';
 import kenyan from '../assets/kenyan.jpeg'
+import contact  from '../assets/contact.png'
 export default{
 bgWhite,
 frontend,
 cyber,
 kenyan,
 developer,
-problem
+problem,
+contact
 
 
 };
