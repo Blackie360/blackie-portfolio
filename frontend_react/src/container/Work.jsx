85007b521574c901e2a545dcf40d4a3a4ed2886c
Felix Jumason <felixkent360@gmail.com>
2023-09-27 23:55:56 +0300

    update

--- frontend_react/src/container/Work.jsx
+++ frontend_react/src/container/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiFillGithub, AiFillEye } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 import { urlFor, client } from '../client';
-import { images } from '../constants'; // Import your icons here
+import { images } from '../constants';
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
@@ -29,26 +29,25 @@ const Work = () => {
       if (item === 'All') {
         setFilterWork(works);
       } else {
-        setFilterWork(works.filter((work) => work.tags.includes(item)));
+        setFilterWork(works.filter((work) => work.tags && work.tags.includes(item)));
       }
     }, 500);
   };
 
   // Icon mapping
   const iconMap = {
-    'UI/UX': images.ui, 
-    'Web App': images.web, 
-    'Mobile App': images.flutter2, 
-    'React JS': images.react, 
-    'All': images.all, 
+    'UI/UX': images.ui,
+    'Web App': images.web,
+    'React JS': images.react,
+    'All': images.all,
   };
 
   return (
-    <div id="portfolio" className='bg-gray-100'>
+    <div id="portfolio" className="bg-gray-100">
       <h2 className="text-3xl gap-2 font-bold mb-4 flex justify-center"><span className='text-orange-500'>Portfolio </span> Section</h2>
 
       <div className="flex space-x-4 text-black justify-center">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
@@ -95,7 +94,9 @@ const Work = () => {
               <p className="text-gray-600">{work.description}</p>
               <div className="flex justify-center mt-4">
                 <div className="app__work-tag bg-gray-300 px-2 py-1 rounded">
-                  <p className="text-sm text-gray-700">{work.tags[1]}</p>
+                  <p className="text-sm text-gray-700 gap-2">
+                    {work.tags && work.tags.length >= 2 ? work.tags[1] : 'No Tag Available'}
+                  </p>
                 </div>
               </div>
             </div>
