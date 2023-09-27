6ebaf986b630f471527ed6dd1a5073835256156c
Felix Jumason <felixkent360@gmail.com>
2023-09-28 00:01:22 +0300

    update

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,7 +2,7 @@ import  { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId:  'hife0g65',
+    projectId:  process.env.REACT_APP_SANITY_PROJECT_ID ,
     dataset:'production',
     apiVersion:'2022-02-01',
     useCdn: true,
